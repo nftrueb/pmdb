dc6eec897b0524991dfcd615c68a77c44f520e4b
nftrueb <nftrueb@gmail.com>
2026-04-06 11:19:45 -0500

    improved repl class organization

--- src/toolshed/cli.py
+++ src/toolshed/cli.py
@@ -4,56 +4,61 @@ from . import get_logger
 
 log = get_logger() 
 
-class Repl: 
-    def __init__(self):
+class Repl:
+
+    # BUILTIN COMMANDS
+    def clear(self): 
+        subprocess.run(['clear']) 
+
+    def quit(self): 
+        self.running = False 
+
+    def print_usage(self): 
+        s = ''
+        s += f'{self.usage}\n'
+        s += f'{self.description}\n'
+
+        s += '\nCOMMANDS:\n'
+        sorted_items = sorted(self.COMM_TO_DESCRIPTION.items(), key=lambda k: k[0])
+        for k, v in sorted_items: 
+            sep = ''.join([' ' for _ in range(self.longest_command + 1 - len(k))]) + '-> '
+            s += f'  {k}{sep}{v}\n'
+
+        # print screen 
+        print(s)
+
+        # check if commands in description map match length of function map 
+        desc_keys = len(self.COMM_TO_DESCRIPTION.keys())
+        func_keys = len(self.COMM_TO_FUNC.keys()) + len(self.BUILTIN_TO_FUNC.keys())
+        if desc_keys !=func_keys: 
+            print('')
+            log.info('Usage screen does not match length of known commands...' +
+                    ' Did you update the usage message after adding a new command?'
+            )
+
+    # BUILTIN CONSTANTS
+    BUILTIN_TO_DESCRIPTION = {
+        'clear' :  'clear the screen (runs the OS clear command)',
+        'help'  :  'prints this screen',
+        'quit'  :  'exit the REPL',
+    }
+    
+    # INIT FUNCTIONS
+    def __init__(self, prompt='> '):
         self.longest_command = 0  
-        self.COMM_TO_FUNC = { }
+        self.COMM_TO_FUNC = {}
         self.COMM_TO_DESCRIPTION = {}
-        self.BUILTIN_TO_FUNC = {
-            'clear': self.clear,
-            'help': self.print_usage, 
-            'quit': self.quit, 
-        }
-        self.BUILTIN_TO_DESCRIPTION = {
-            'clear' :  'clear the screen (runs the OS clear command)',
-            'help'  :  'prints this screen',
-            'quit'  :  'exit the REPL',
-        }
+        
         self.usage = ''
         self.description = ''
         self.running = False
 
-    def is_init(self): 
-        return (len(self.COMM_TO_DESCRIPTION.items()) != 0
-            and len(self.COMM_TO_FUNC.items()) != 0
-            and self.usage != ''
-            and self.description != ''
-        )
-
-    def run(self): 
-        self.running = True
-        try: 
-            while self.running: 
-                self.handle_command()
-                
-        except Exception as ex: 
-            log.error('Error encountered in repl loop', ex) 
-
-    def handle_command(self): 
-        command = [ word.strip() for word in input('> ').split() ]
-        print(f'Command: {command}')
-
-        if len(command) == 0 or command[0].isspace(): 
-            return 
-
-        if command[0] in self.COMM_TO_FUNC.keys(): 
-            self.COMM_TO_FUNC[command[0]](command)
-
-        elif command[0] in self.BUILTIN_TO_FUNC.keys(): 
-            self.BUILTIN_TO_FUNC[command[0]]()
-
-        else: 
-            log.error(f'Failed to parse command')
+        self.prompt = prompt
+        self.BUILTIN_TO_FUNC = {
+        'clear': self.clear,
+        'help': self.print_usage, 
+        'quit': self.quit, 
+    }
 
     def register_commands(self, funcs, descriptions): 
         self.COMM_TO_FUNC = funcs 
@@ -67,29 +72,34 @@ class Repl:
         self.usage = usage
         self.description = description
 
-    def print_usage(self): 
-        s = ''
-        s += f'{self.usage}\n'
-        s += f'{self.description}\n'
+    def is_init(self): 
+        return (len(self.COMM_TO_DESCRIPTION.items()) != 0
+            and len(self.COMM_TO_FUNC.items()) != 0
+            and self.usage != ''
+            and self.description != ''
+        )
 
-        s += '\nCOMMANDS:'
-        sorted_items = sorted(self.COMM_TO_DESCRIPTION.items(), key=lambda k: k[0])
-        for k, v in sorted_items: 
-            sep = ''.join([' ' for _ in range(self.longest_command + 1 - len(k))]) + '-> '
-            s += f'  {k}{sep}{v}'
+    # RUNNING FUNCTIONS
+    def run(self): 
+        self.running = True
+        try: 
+            while self.running: 
+                self.handle_command()
+                
+        except Exception as ex: 
+            log.error('Error encountered in repl loop', ex) 
 
-        # print screen 
-        print(s)
+    def handle_command(self): 
+        command = [ word.strip() for word in input(self.prompt).split() ]
 
-        # check if commands in description map match length of function map 
-        if len(self.COMM_TO_FUNC.keys()) != len(self.COMM_TO_DESCRIPTION.keys()): 
-            print('')
-            log.info('Usage screen does not match length of known commands...' +
-                    ' Did you update the usage message after adding a new command?'
-            )
+        if len(command) == 0 or command[0].isspace(): 
+            return 
 
-    def clear(self): 
-        subprocess.run(['clear']) 
+        if command[0] in self.COMM_TO_FUNC.keys(): 
+            self.COMM_TO_FUNC[command[0]](command)
 
-    def quit(self): 
-        self.running = False 
+        elif command[0] in self.BUILTIN_TO_FUNC.keys(): 
+            self.BUILTIN_TO_FUNC[command[0]]()
+
+        else: 
+            log.error(f'Failed to parse command')
